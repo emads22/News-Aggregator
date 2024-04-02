52771a83823e2af7ad87967bf1e4f418768911e6
emads22 <emadsaab722@gmail.com>
2024-04-02 16:28:47 +0300

    send email with these titles and descriptions

--- main.py
+++ main.py
@@ -4,25 +4,40 @@ import os
 from dotenv import load_dotenv
 
 
-# Load environment variables from the .env file
-load_dotenv()
+def main():
+    # Load environment variables from the .env file
+    load_dotenv()
 
-# Define the endpoint and API key for the News API, and the topic we want to search for news
-NEWS_API_ENDOINT_EVERYTHING = "https://newsapi.org/v2/everything"
-NEWS_API_KEY = os.getenv("NEWS_apiKey")
-TOPIC = "tesla"
+    # Define the endpoint and API key for the News API, and the topic we want to search for news
+    NEWS_API_ENDOINT_EVERYTHING = "https://newsapi.org/v2/everything"
+    NEWS_API_KEY = os.getenv("NEWS_apiKey")
+    TOPIC = "tesla"
 
-# Make a GET request to the News API to fetch news articles related to the specified topic wth arguments ('q' and 'apiKey')
-request = requests.get(f"{NEWS_API_ENDOINT_EVERYTHING}?q={TOPIC}&apiKey={NEWS_API_KEY}")
-# Using `request.json()` is better to access data using familiar Python syntax, such as dictionary keys and list indices than using `request.text` which is a plain string (str type)
-content = request.json()
+    # Make a GET request to the News API to fetch news articles related to the specified topic wth arguments ('q' and 'apiKey')
+    request = requests.get(f"{NEWS_API_ENDOINT_EVERYTHING}?q={TOPIC}&apiKey={NEWS_API_KEY}")
+    # Using `request.json()` is better to access data using familiar Python syntax, such as dictionary keys and list indices than using `request.text` which is a plain string (str type)
+    content = request.json()
 
-# Use Debugging mode to monito data variables and how they are structures (specially lists and dicts) so we know articles are stored in dict content of key 'articles' and same thing for 'title' aand 'description'
-for article in content['articles']:
-    # Create a text block containing title, description, and content - Content: {article['content']}
-    article_data = f"""
-    - Title: {article['title']}
-    - Description: {article['description']}
-    
-    """
-    print(article_data)
+    # define the text block of all articles to be snt by email
+    all_articles_data = ""
+
+    # Use Debugging mode to monito data variables and how they are structures (specially lists and dicts) so we know articles are stored in dict content of key 'articles' and same thing for 'title' aand 'description'
+    for article in content['articles']:
+        # Check if title is not None
+        if article['title'] is not None:
+            # Create a text block containing title, description of each article of the topic selected and concatenated to all articles text block
+            all_articles_data += f"""
+- Title: {article['title']}
+- Description: {article['description']}
+"""
+
+    # send an email of all these articles text block and handle the outcome
+    subject = f"{TOPIC.title()} News: The Latest Updates and Headlines"
+    if send_email(all_articles_data, subject):
+        print("\n--- Email sent successfully ---\n")
+    else:
+        print("\n--- Failed to send email. Please try again later ---\n")
+
+
+if __name__ == "__main__":
+    main()
--- send_email.py
+++ send_email.py
@@ -4,46 +4,35 @@ import os
 from dotenv import load_dotenv
 
 
-def send_email(message):
-    # Load environment variables from the .env file
+def send_email(message, subject):
+
     load_dotenv()
-    # Access the environment variables
     username = os.getenv("USER")
     password = os.getenv("PASSWORD")
     receiver = os.getenv("RECEIVER")
-    # Define SMTP server settings, SMTP server hostname and SMTP server port (SSL)
+
     host = "smtp.gmail.com"
     port = 465
-    # SSL (Secure Sockets Layer): SSL is a protocol used to secure communication over a computer network. It provides encryption, authentication, and data integrity to ensure that data transferred between a client and a server remains confidential and tamper-proof.
-    # ssl.create_default_context() creates an SSL context object (context) to establish secure connections over a network, such as when making HTTPS requests or setting up secure socket connections (secure communication)
     context = ssl.create_default_context()
-    # Create email message (Subject must come directly after """ then add breakline before acual message)
-    message = f"""Subject: New Email from {username}
+
+    # encoding the message as UTF-8 to ensure that all characters are properly encoded and can be sent without causing encoding errors
+    message = f"""Subject: {subject}
 
     {message}
-    """
+    """.encode("utf-8")
 
     try:
-        # smtplib.SMTP_SSL() establish an SMTP connection to SMTP server over SSL. It ensures that the communication between this application and the SMTP server is encrypted and secure.
         with smtplib.SMTP_SSL(host, port, context=context) as server:
-            # Login to SMTP server
             server.login(username, password)
-            # Send email from sender to receiver
             server.sendmail(username, receiver, message)
-        # # Display success message
-        # print("\nEmail sent successfully\n")  
         # Return True to indicate successful email sending
         return True
     
-    except stmlib.SMTPException as e:
-        # # Display error message if email sending fails  
-        # print(f"\nFailed to send email: {e}\n")  
+    except smtplib.SMTPException as e:
         # Log the error for debugging purposes
         # logging.error(f"Failed to send email: {e}")
         # Return False to indicate email sending failure
         return False
+        
 
-
-
-# this_messsage = "HELLO THERE! HOW ARE YOU??"
-# send_email(this_messsage)
+    
