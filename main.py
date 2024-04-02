f79aacb12c585a0620ddf2a9b4b7baefbf6eb0df
emads22 <emadsaab722@gmail.com>
2024-04-02 17:32:31 +0300

    improve email with adding links, limiting news, setting english language, dynamic endpoint, and adding subject

--- main.py
+++ main.py
@@ -12,24 +12,28 @@ def main():
     NEWS_API_ENDOINT_EVERYTHING = "https://newsapi.org/v2/everything"
     NEWS_API_KEY = os.getenv("NEWS_apiKey")
     TOPIC = "tesla"
+    LANGUAGE = "en"
 
-    # Make a GET request to the News API to fetch news articles related to the specified topic wth arguments ('q' and 'apiKey')
-    request = requests.get(f"{NEWS_API_ENDOINT_EVERYTHING}?q={TOPIC}&apiKey={NEWS_API_KEY}")
+    # Make a GET request to the News API to fetch news articles related to the specified topic wth parameters ('q' , 'language', and 'apiKey')
+    request = requests.get(f"{NEWS_API_ENDOINT_EVERYTHING}?q={TOPIC}&language={LANGUAGE}&apiKey={NEWS_API_KEY}")
     # Using `request.json()` is better to access data using familiar Python syntax, such as dictionary keys and list indices than using `request.text` which is a plain string (str type)
     content = request.json()
 
-    # define the text block of all articles to be snt by email
+    # define the text block of all articles to be sent by email and total number of articles 
     all_articles_data = ""
+    total_articles = 0
 
     # Use Debugging mode to monito data variables and how they are structures (specially lists and dicts) so we know articles are stored in dict content of key 'articles' and same thing for 'title' aand 'description'
-    for article in content['articles']:
+    for article in content['articles'][:20]: # only collect the 20 first articles
         # Check if title is not None
         if article['title'] is not None:
             # Create a text block containing title, description of each article of the topic selected and concatenated to all articles text block
             all_articles_data += f"""
-- Title: {article['title']}
-- Description: {article['description']}
-"""
+{article['title']}:
+{article['description']}
+{article['url']}
+
+"""         
 
     # send an email of all these articles text block and handle the outcome
     subject = f"{TOPIC.title()} News: The Latest Updates and Headlines"
